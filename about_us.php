457f608ab9eec50f2856e3961e342f4db6cf0c9f
SAKETH933 <57320746+SAKETH933@users.noreply.github.com>
2020-11-08 14:48:20 +0300

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,137 @@
+<?php
+include("includes/head.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<title>About Us</title>
+    <style type="text/css">
+
+         .ART1 {
+         margin: 110px 0px 0px -60px;
+          font-size: 90px;
+          font-family: "Yu Gothic UI";
+          color: rgb( 45, 112, 213 );
+          text-align: center;
+          position: absolute;
+          top: -15px;
+          width: 215px;
+          height: 35px;
+          z-index: 19;
+        }
+
+        ._BAY1 {
+            margin: 110px 0px 0px 120px;
+          font-size: 90px;
+          font-family: "Yu Gothic UI";
+          color: rgb( 20, 20, 20);
+          text-align: center;
+          position: absolute;
+          top: -15px;
+          width: 215px;
+          height: 35px;
+          z-index: 19;
+        }
+
+        .Albay_1 {
+          margin: 250px 0px 0px 99px;
+          font-size: 30px;
+          font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+           z-index: 3;
+        }
+
+        .Original_1 {
+          margin: 250px 0px 0px 190px;
+          font-size: 30px;
+         font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+           z-index: 3;
+        }
+
+        .Artworks_1{
+
+          margin: 250px 0px 0px 325px;
+          font-size: 30px;
+          font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+          z-index: 3;
+        }
+          .border1{
+          border-radius: 8px;
+        float: left;
+        border: 1px solid none;
+        background-color: #112233;
+        padding: 150px 490px 0px 60px;
+        margin-left: 350px;
+        margin-top: 100px;
+
+    }
+     .border2{
+          border-radius: 8px;
+        float: left;
+        border: 1px solid none;
+        background-color: #112233;
+        padding: 420px 0px 0px 1160px;
+        margin-left: 50px;
+        margin-top: -20px;
+        margin-bottom: 0px;
+        position: absolute;
+        top:250px;
+        bottom: 0px;
+        opacity: 0.5;
+
+    }
+    .text1{
+            position: absolute;
+            color: rgb(255, 255, 255);
+            margin: 10px 5px 105px 5px;
+            text-align: justify;
+            font-family: "Yu Gothic UI Light";
+            font-weight: normal;
+            font-size: 20px;
+            top: 40px;
+            left: 50px;
+            right: 50px;
+            z-index: 3;
+
+        }
+    </style>
+
+<body>
+    <div class="border1">
+          <h1 class="ART1">ART </h1>
+          <h1 class="_BAY1">EASY </h1>
+          <h2 class="Albay_1">Quite </h2>
+          <h2 class="Original_1">Original </h2>
+          <h2 class="Artworks_1">Artworks </h2>
+  </div>
+  <div class="border2">
+
+
+<p class="text1" >ART EASY Albay Original Artworks Gallery, located in the Vellore Institute of Technology.
+
+<br><br>ART EASY Albay Original Artworks Gallery maintains four keys areas of focus: exhibitions, education, residency and consultation.
+
+<br><br>ART EASY curated exhibitions provide audiences with an opportunity to experience different approaches and execution styles of art. The artists, both local and international, are often provided the opportunity to exhibit the work they have created at ART+BAY at the end of their residency. ART EASY also provides qualified art consultation services for its discerning collectors, building on and enhancing existing collections, as well as helping establish new collections.
+
+<br><br>Through these initiatives, ART EASY maintains its commitment to dynamically engage and add value to the local and international art communities.</p><br><br><br><br><br>
+ </div>
+<br><br><br><br>
+  
+</body>
+</html>
